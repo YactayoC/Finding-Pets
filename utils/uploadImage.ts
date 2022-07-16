7f4f1c754262fefdb8bb7b3066ada6fbbeebfbdf
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-07-16 15:51:40 -0500

    FEAT: add types to upload image service

--- utils/uploadImage.ts
+++ utils/uploadImage.ts
@@ -1,17 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const uploadImage = async (file: File) => {
-  const cloudUrl = 'https://api.cloudinary.com/v1_1/sebas-2001-yac/upload';
+import { CloudinaryResponse } from "types/cloudinaryResponse";
+
+export const uploadImage = async (
+  file: File
+): Promise<CloudinaryResponse | null> => {
+  const cloudUrl = "https://api.cloudinary.com/v1_1/sebas-2001-yac/upload";
   const formData = new FormData();
-  formData.append('upload_preset', 'finding-pets');
-  formData.append('file', file);
+  formData.append("upload_preset", "finding-pets");
+  formData.append("file", file);
 
   try {
-    const resp = await axios.post(cloudUrl, formData);
-
-    const cloudResp = await resp.data;
-    return cloudResp;
+    const { data } = await axios.post<CloudinaryResponse>(cloudUrl, formData);
+    return data;
   } catch (error) {
     console.log(error);
+    return null;
   }
 };
