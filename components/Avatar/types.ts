9b9d494cdb4cb9582b4a16bda68c29bebffa7ee6
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-07-16 19:29:18 -0500

    FEAT: create avatar component

---
+++ components/Avatar/types.ts
@@ -0,0 +1,12 @@
+import type { ImageProps } from "next/image";
+
+export type AvatarSize = "small" | "normal" | "medium" | "large";
+export type AvatarProps = {
+  rounded?: boolean;
+  size?: AvatarSize;
+  src?: string;
+  editable?: boolean;
+  alt?: string;
+  email: string;
+  onConfirmUpload?: (image: File) => void;
+} & ImageProps;
