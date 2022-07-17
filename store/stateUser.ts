d86387c54a3e7b80284e5b841acb4533b40aec2b
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-07-16 20:09:49 -0500

    FEAT: create mutation to upadate avatar

--- store/stateUser.ts
+++ store/stateUser.ts
@@ -1,4 +1,10 @@
-import { atom } from 'jotai';
-import { TUser } from 'types';
+import { atom } from "jotai";
+import { TUser } from "types";
 
 export const infoUser = atom<TUser | null>(null);
+
+export const updateAvatarAtom = atom(null, async (get, set, avatar: string) => {
+  const user = get(infoUser);
+  if (!user) return;
+  set(infoUser, { ...user, profile: avatar });
+});
