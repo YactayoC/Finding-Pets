e9994a2df7c3b6648e963586edf89e146960fb18
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-07-19 20:05:32 -0500

    FEAT: add components to edit user data

--- pages/home/profile/[id].tsx
+++ pages/home/profile/[id].tsx
@@ -3,14 +3,26 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useAtomValue, useUpdateAtom } from "jotai/utils";
 
-import ProfileLayout from "components/layouts/Home/ProfileLayout";
-import HomePublications from "components/homeUI/HomePublications";
-import LoaderProfile from "../../../components/loader/LoaderProfile";
-import { dateUser } from "utils/dateInfo";
-import { useSWRPublications, useSWRUser } from "hooks";
-import findingPetsApi from "api/findingPetsApi";
+//store
 import { stateModalProfile } from "store/stateModalProfile";
 import { infoUser } from "store/stateUser";
+
+//components
+import ProfileLayout from "components/layouts/Home/ProfileLayout";
+import HomePublications from "components/homeUI/HomePublications";
+import Avatar from "components/Avatar";
+import LoaderProfile from "../../../components/loader/LoaderProfile";
+import EditableField from "components/EditableField";
+//utils
+import { dateUser } from "utils/dateInfo";
+import { useSWRPublications, useSWRUser } from "hooks";
+import { useUploadAvatar } from "hooks/user/mutations/useUploadAvatar";
+import { useChangeFullName } from "hooks/user/mutations/useChangeFullName";
+import { useChangePhone } from "hooks/user/mutations/useChangePhone";
+
+//services
+
+import findingPetsApi from "api/findingPetsApi";
 
 import styles from "styles/home/Profile.module.css";
 
@@ -23,12 +35,17 @@ const ProfilePage = ({ userSSR }: any) => {
   const { user } = useSWRUser(`/user/get-user?userId=${userAtom?._id}`);
   const setShowModalEditUser = useUpdateAtom(stateModalProfile);
 
+  const { uploadAvatar } = useUploadAvatar();
+  const { changeFullName } = useChangeFullName();
+  const { changePhone } = useChangePhone();
+
   if (!userSSR) return;
   if (!user) {
     return null;
   }
 
   const userData = user.user._id === userSSR._id ? user.user : userSSR;
+
   return (
     <ProfileLayout title={`Perfil | ${userData.fullname}`}>
       <div className={styles.profile}>
@@ -37,20 +54,23 @@ const ProfilePage = ({ userSSR }: any) => {
             {isLoading ? (
               <LoaderProfile />
             ) : (
-              <Image
+              <Avatar
                 src={userData.profile}
-                width={180}
-                height={180}
                 alt="profile"
+                editable={true}
+                size="large"
+                email={userData.email}
+                onConfirmUpload={uploadAvatar}
               />
             )}
+
             <div className={styles.option}>
               <h2>{userData.fullname}</h2>
-              {userSSR._id === user?.user?._id && (
+              {/* {userSSR._id === user?.user?._id && (
                 <button onClick={() => setShowModalEditUser(true)}>
                   <i className="fa-solid fa-pen-to-square"></i>Editar Perfil
                 </button>
-              )}
+              )} */}
             </div>
           </div>
         </div>
@@ -60,18 +80,25 @@ const ProfilePage = ({ userSSR }: any) => {
             <h2>Detalles</h2>
 
             <ul className={styles.details__data}>
-              <li>
-                <i className="fa-solid fa-user"></i>
-                {userData.fullname}
-              </li>
-              <li>
-                <i className="fa-solid fa-at"></i>
-                {userData.email}
-              </li>
-              <li>
-                <i className="fa-solid fa-phone"></i>
-                {userData.phone}
-              </li>
+              <EditableField
+                value={userData.fullname}
+                icon={<i className="fa-solid fa-user"></i>}
+                onConfirm={changeFullName}
+              />
+              <EditableField
+                value={userData.email}
+                icon={<i className="fa-solid fa-at"></i>}
+                type="email"
+                onConfirm={async () => {
+                  throw new Error("error");
+                }}
+              />
+              <EditableField
+                value={userData.phone}
+                icon={<i className="fa-solid fa-phone"></i>}
+                type="tel"
+                onConfirm={changePhone}
+              />
               <li>
                 <i className="fa-solid fa-bullhorn"></i> Se unió{" "}
                 {dateUser(userSSR?.createdAt)}
